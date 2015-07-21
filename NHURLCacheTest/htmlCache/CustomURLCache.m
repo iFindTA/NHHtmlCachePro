658534d33c1cbb69614fe4a30fbd6d6c81765f91
hu jiaju <nanhujiaju@163.com>
2015-07-21 10:52:28 +0800

    remove md5 single file

--- NHURLCacheTest/htmlCache/CustomURLCache.m
+++ NHURLCacheTest/htmlCache/CustomURLCache.m
@@ -8,6 +8,46 @@
 
 #import "CustomURLCache.h"
 #import "Reachability.h"
+#import <CommonCrypto/CommonCrypto.h>
+
+@interface NSString (helper)
++ (NSString *)sha1:(NSString *)str;
++ (NSString *)md5Hash:(NSString *)str;
+@end
+
+@implementation NSString(helper)
++ (NSString *)sha1:(NSString *)str {
+    const char *cstr = [str cStringUsingEncoding:NSUTF8StringEncoding];
+    NSData *data = [NSData dataWithBytes:cstr length:str.length];
+    
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
+    
+    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+    
+    return output;
+}
+
++ (NSString *)md5Hash:(NSString *)str {
+    const char *cStr = [str UTF8String];
+    unsigned char result[16];
+    CC_MD5( cStr, (CC_LONG)strlen(cStr), result );
+    NSString *md5Result = [NSString stringWithFormat:
+                           @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+                           result[0], result[1], result[2], result[3],
+                           result[4], result[5], result[6], result[7],
+                           result[8], result[9], result[10], result[11],
+                           result[12], result[13], result[14], result[15]
+                           ];
+    return md5Result;
+}
+
+@end
 
 @interface CustomURLCache(private)
 
@@ -91,11 +131,11 @@
 }
 
 - (NSString *)cacheRequestFileName:(NSString *)requestUrl {
-    return [Util md5Hash:requestUrl];
+    return [NSString md5Hash:requestUrl];
 }
 
 - (NSString *)cacheRequestOtherInfoFileName:(NSString *)requestUrl {
-    return [Util md5Hash:[NSString stringWithFormat:@"%@-otherInfo", requestUrl]];
+    return [NSString md5Hash:[NSString stringWithFormat:@"%@-otherInfo", requestUrl]];
 }
 
 - (NSCachedURLResponse *)dataFromRequest:(NSURLRequest *)request {
